36089b2ee04309115bb01784aea7f4e3dd715919
Hermann Elton <60223374+Her0elt@users.noreply.github.com>
2020-04-22 14:51:51 +0200

    Update Oving2Oppg2.java

--- oving 2/Oving2Oppg2.java
+++ oving 2/Oving2Oppg2.java
@@ -1,15 +1,14 @@
-//Programmet tester hvilket av de to Kj¯tt pakkene er billigst
+//Programmet tester hvilket av de to Kj√∏tt pakkene er billigst
 class Oving2Oppg2{
 	public static void main(String[]args){
 		double kA=35.90;
 		double kB=39.50;
 		double kjottAPrG=(kA/450);
 		double kjottBPrG=(kB/500);
-
-		if(kjottAPrG>kjottBPrG){
-			System.out.println("Kj¯tt A koster mer pr kg enn Kj¯tt B derfor er B billigere enn A");
-		}else{
-			System.out.println("Kj¯tt A koster mindre pr kg enn Kj¯tt B derfor er A billigere enn B");
-		}
+	
+		String message = (kjottAPrG>kjottBPrG)? "Kj√∏tt A koster mer pr kg enn Kj√∏tt B derfor er B billigere enn A":
+		"Kj√∏tt A koster mindre pr kg enn Kj√∏tt B derfor er A billigere enn B"; 
+		
+		System.out.println(message);
 	}
 }
